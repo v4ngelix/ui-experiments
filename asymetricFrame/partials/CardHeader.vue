d3e38fdff4d8330045654afe325816c74a3845c8
Andi Angel <andi.angel@reach-u.com>
2021-07-03 00:08:39 +0300

    Initial commit

---
+++ asymetricFrame/partials/CardHeader.vue
@@ -0,0 +1,20 @@
+Vue.component('CardHeader', {
+  /*
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  */
+  props: ['establishment'],
+  template: '
+    <card-header>
+      <h1>
+        Food Menu
+      </h1>
+      <h2>
+        {{establishment}}
+      </h2>
+    </card-header>
+  '
+});
