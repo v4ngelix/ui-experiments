e7cac2397ddf9ae8253eb39c353c756e152f6965
Andi Angel <andi.angel@boheemia.ee>
2026-06-24 15:45:44 +0300

    Indent code

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ async function initializePage() {
   document.getElementById("description").innerHTML = await readmeFile.text();
 }
 
-initializePage().catch((error) => {
-  const main = document.getElementsByTagName("main")?.[0];
-  console.log("catch", error.message, main);
-  if (main) {
-    main.classList.add("error");
-    main.innerHTML = error.message;
-  }
-}).finally(() => {
-  console.log("finally");
-  const spinner = document.getElementById("spinner");
-  if (spinner) {
-    spinner.classList.add("has-loaded");
-    setTimeout(() => {
-      document.body.removeChild(spinner);
-    },351);
-  }
-});
+initializePage()
+  .catch((error) => {
+    const main = document.getElementsByTagName("main")?.[0];
+    if (main) {
+      main.classList.add("error");
+      main.innerHTML = error.message;
+    }
+  }).finally(() => {
+    const spinner = document.getElementById("spinner");
+    if (spinner) {
+      spinner.classList.add("has-loaded");
+      setTimeout(() => {
+        document.body.removeChild(spinner);
+      },351);
+    }
+  });
